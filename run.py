12be5cb3d76f2975770af5cbba3bb6ffd9fe454d
cristianmtr <cristi.mtr@gmail.com>
2015-06-16 15:06:31 +0200

    client-side js cache

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from models import create_session, create_task_object, create_user_object
 import logging
 from logging.handlers import RotatingFileHandler
+# debugging only
+import pprint
+
 
 app = Flask(__name__)
 
@@ -13,8 +16,6 @@ class Globals(object):
         self.task = create_task_object()
         self.user = create_user_object()
 
-globals = Globals()
-        
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -37,12 +38,20 @@ def json():
             "Hulk"
         ]
     ]
-    app.logger.warning('data = {}'.format(data))
-    return jsonify(draw=0, recordsTotal=2, recordsFiltered=2, data=data)
+
+    # some debug info
+    app.logger.info('data = {}'.format(data))
+    args = dict(request.args)
+    app.logger.info(args.keys())
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(args)
+    
+    return jsonify(data=data)
 
 if __name__ == "__main__":
     app.debug = True
     handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
+    globals = Globals()
     app.run(host='0.0.0.0', port=5000)
