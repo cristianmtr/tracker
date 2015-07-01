311995b890731d85424743c3f07a5b4294a4e5d1
cristianmtr <cristi.mtr@gmail.com>
2015-07-01 14:22:55 +0200

    date picker w/ momentjs

--- static/main.js
+++ static/main.js
@@ -1,42 +1,56 @@
 var dataSet;
 
-$(document).ready(function() {
-    $.fn.editable.defaults.mode = 'inline';         
+$(document).ready(function () {
+    $.fn.editable.defaults.mode = 'inline';
 
-    $(function() {
-    	$('#priority').editable({
-	    type: 'select',
-	    title: 'Priority',
-	    placement: 'right',
-    	    value: 2,    
-    	    source: [
-    		  {value: 1, text: 'Urgent'},
-    		  {value: 2, text: 'Medium'},
-    		  {value: 3, text: 'Low'}
-    	       ]
-	    
-    	});
+    $('#priority').editable({
+        type: 'select',
+        title: 'Priority',
+        placement: 'right',
+        value: 2,
+        source: [
+            {value: 1, text: 'Urgent'},
+            {value: 2, text: 'Medium'},
+            {value: 3, text: 'Low'}
+        ]
+
     });
-    
+
+    $('#deadline').editable({
+        type: 'combodate',
+	format: 'DD.MM.YYYY',
+	title: 'Deadline',
+        viewformat: 'DD.MMMM.YYYY',    
+        template: 'DD.MMMM.YYYY',
+	value: moment().format(new moment().format("DD.MM.YYYY")),
+        combodate: {
+                minYear: 2015,
+                maxYear: 2016,
+                minuteStep: 1
+           }
+	
+    })
+
+
     function test() {
-	return $.getJSON('/json');
+        return $.getJSON('/json');
     }
 
-    $.when(test()).then(function(data) {
-	dataSet = data['data'];
-	$('#example').DataTable({
-	    "data" : dataSet,
-	    "columns" : [ {
-		"title" : "id"
-	    }, {
-		"title" : "description"
-	    }, {
-		"title" : "dead line"
-	    }, {
-		"title" : "responsible"
-	    }, {
-		"title" : "author"
-	    } ]
-	});
+    $.when(test()).then(function (data) {
+        dataSet = data['data'];
+        $('#example').DataTable({
+            "data": dataSet,
+            "columns": [{
+                "title": "id"
+            }, {
+                "title": "description"
+            }, {
+                "title": "dead line"
+            }, {
+                "title": "responsible"
+            }, {
+                "title": "author"
+            }]
+        });
     });
 });
