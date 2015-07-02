63f2642c7878d9920fc762fec02aea447af43881
cristianmtr <cristi.mtr@gmail.com>
2015-07-02 11:21:59 +0200

    table with editable in create new

--- static/main.js
+++ static/main.js
@@ -26,6 +26,57 @@ function insertNewRowEmpty() {
 
 $(document).ready(function () {
 
+    $.fn.editable.defaults.mode = 'inline';
+
+    $('#priority').editable({
+        type: 'select',
+        title: 'Priority',
+        placement: 'right',
+        value: 2,
+        source: [
+            {value: 1, text: 'Urgent'},
+            {value: 2, text: 'Medium'},
+            {value: 3, text: 'Low'}
+        ]
+
+    });
+
+    $('#tasklist').editable({
+        type: 'select',
+        title: 'Task list',
+        placement: 'right',
+        value: 3,
+        source: [
+            {value: 1, text: 'Dummy'},
+            {value: 2, text: 'Fake'},
+            {value: 3, text: 'Foo'},
+            {value: 4, text: 'Bar'},
+        ]
+    });
+
+    $("#title").editable({
+        type: 'text',
+        title: 'Title',
+        clear: true,
+        placeholder: 'task title',
+    });
+
+    $("#description").editable({
+        type: 'textarea',
+        escape: true,
+    });
+
+    $("#responsible").editable({
+        type: 'select',
+        value: 1,
+        title: 'Responsible',
+        placement: 'right',
+        source: [
+            {value: 1, text: 'Cristian'},
+            {value: 2, text: 'Someone else'},
+        ]
+    });
+
     function test() {
         return $.getJSON('/json');
     }
@@ -47,7 +98,6 @@ $(document).ready(function () {
             }]
         });
     });
-
 
 
 });
