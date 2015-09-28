69b27bd495ae8bbf1106c5900262e299cc2d6b4b
cristianmtr <cristi.mtr@gmail.com>
2015-09-28 11:32:51 +0200

    using Sessionmaker factory instead of global single session; fix: mysql session no longer timing out;

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
 import json
 import sys
 
@@ -9,9 +9,9 @@ def create_engine_from_config(config_file="config.json"):
     with open(config_file) as config_file_handle:
         config = json.load(config_file_handle)
         print "Using config:"
-        print json.dumps(config, sort_keys=False, indent=4, separators=(',',': '))
-    engine = create_engine('mysql+mysqlconnector://%s:%s@%s:%s/%s' %(config['username'],config['password'], config['host'], config['port'], config['database']),
-                           pool_recycle=3600)
+        print json.dumps(config, sort_keys=False, indent=4, separators=(',', ': '))
+    engine = create_engine('mysql+mysqlconnector://%s:%s@%s:%s/%s' % (
+    config['username'], config['password'], config['host'], config['port'], config['database']))
     return engine
 
 
@@ -23,14 +23,15 @@ class Globals(object):
         Base = automap_base()
         engine = create_engine_from_config()
         Base.prepare(engine, reflect=True)
-        self.session = Session(engine)
+        self.session = sessionmaker()
+        self.session.configure(bind=engine)
         self.task = Base.classes.frk_item
         self.user = Base.classes.frk_member
         self.tasklist = Base.classes.frk_project
         self.comment = Base.classes.frk_itemComment
         self.history = Base.classes.frk_itemStatus
 
-    def try_commit(self):
+    def try_commit(self, session):
         """
         tries to commit session as is
         :rtype : int
@@ -38,9 +39,9 @@ class Globals(object):
         - 1 on fail
         """
         try:
-            self.session.commit()
+            session.commit()
             return 0
         except Exception as e:
             print >> sys.stderr, 'ERROR COMMITTING DB: {}\nROLLING BACK'.format(e)
-            self.session.rollback()
+            session.rollback()
             return -1
